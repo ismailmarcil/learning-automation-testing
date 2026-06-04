8363ad8ab58eb03a3afb13ec295a377c885f167b
ismailmarcil <marcilismaill@gmail.com>
2026-06-04 12:40:51 +0100

    project structure

---
+++ src/test/java/steps/AuthSteps.java
@@ -0,0 +1,10 @@
+package steps;
+
+import net.serenitybdd.annotations.Step;
+import static net.serenitybdd.rest.SerenityRest.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthSteps {
+
+}
